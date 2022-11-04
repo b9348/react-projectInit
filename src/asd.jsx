b025949212e089afaf445474d38ffbec3d3d3e54
yangfengyuan-ht <bb9348@163.com>
2022-11-04 17:59:59 +0800

    axios update

--- src/asd.jsx
+++ src/asd.jsx
@@ -1,8 +1,35 @@
-import { Input } from 'antd';
-import React from 'react';
+import { Input, Button } from 'antd';
+import React, { useEffect } from 'react';
+import { postF } from './utils/api'
+import './asd.less';
+
 const Asd = () => {
-    return <>
-        <Input placeholder="Basic usage" />
-    </>
+    useEffect(() => {
+        const query = async () => {
+            const res = await postF()
+            console.log(res);
+        }
+        query()
+    }, [])
+    return <div className="wrapper">
+        <div className="log">
+            <div className="title">glad to see u</div>
+            <div className="desc">登录以继续</div>
+
+            <div className="inputHead">
+                <div className="head">Email</div>
+                <Input />
+            </div>
+            <div className="inputHead">
+                <div className="head">
+                    <div className="pwd">Password</div>
+                    <Button type="link">forget ur pwd?</Button>
+                </div>
+                <Input />
+            </div>
+
+            <Button type="primary" >Login</Button>
+        </div>
+    </div>
 };
 export default Asd;
